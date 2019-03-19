b6c9286c5978742fa43d97d90e36fb7cca4f0e99
nikunj <nikupatel199@gmail.com>
2019-03-18 20:51:58 -0500

    schedular

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Home from "./pages/Home";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
+import Scheduler from "./pages/Scheduler";
+import Login from "./pages/Login";
 
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/home" component={Home} />
+          <Route exact path="/scheduler" component={Scheduler} />
+          <Route exact path="/login" component={Login} />          
           <Route component={NoMatch} />
         </Switch>
       </div>
