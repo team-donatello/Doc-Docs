20609adbe78d213b96a4b77ef37315acfd31bc9e
Mark Langhamer <langhamerm@gmail.com>
2019-03-23 10:31:38 -0500

    Patient Schema

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const doctorRoutes = require("./doctors");
-
-// Book routes
+const patientRoutes = require("./patients");
+// API routes
 router.use("/doctors", doctorRoutes);
+router.use("/patients", patientRoutes);
 
 
 module.exports = router;
