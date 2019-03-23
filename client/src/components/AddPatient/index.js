8d27845a769e19df93703740e3655c3983f84d7a
echavez85 <echavez85@gmail.com>
2019-03-23 12:57:15 -0500

    started patient add form

--- client/src/components/AddPatient/index.js
+++ client/src/components/AddPatient/index.js
@@ -13,26 +13,41 @@ function PatientForm({ children }) {
                     <label for="exampleFormControlInput1">Age</label>
                     <input type="age" class="form-control" id="exampleFormControlInput1" placeholder="21">
                 </div>
+
                 <div class="form-group">
-                    <label for="exampleFormControlSelect1">Example select</label>
+                    <label for="exampleFormControlSelect1">Gender</label>
                     <select class="form-control" id="exampleFormControlSelect1">
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
+                        <option>Male</option>
+                        <option>Female</option>
+                        <option>Other</option>
                     </select>
                 </div>
+
                 <div class="form-group">
-                    <label for="exampleFormControlSelect2">Example multiple select</label>
-                    <select multiple class="form-control" id="exampleFormControlSelect2">
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                    </select>
-  </div>
+                    <label for="exampleFormControlInput1">Height in Inches</label>
+                    <input type="height" class="form-control" id="exampleFormControlInput1" placeholder="60">
+                </div>
+
+                <div class="form-group">
+                    <label for="exampleFormControlInput1">Weight in Lbs</label>
+                    <input type="weight" class="form-control" id="exampleFormControlInput1" placeholder="180">
+                </div>
+
+                <div class="form-group">
+                    <label for="exampleFormControlInput1">Heartrate</label>
+                    <input type="heartrate" class="form-control" id="exampleFormControlInput1" placeholder="75">
+                </div>
+
+                <div class="form-group">
+                    <label for="exampleFormControlInput1">Blood Pressure</label>
+                    <input type="bloodpressure" class="form-control" id="exampleFormControlInput1" placeholder="110/83">
+                </div>
+
+                <div class="form-group">
+                    <label for="exampleFormControlInput1">Medications</label>
+                    <input type="age" class="form-control" id="exampleFormControlInput1" placeholder="21">
+                </div>
+
   <div class="form-group">
     <label for="exampleFormControlTextarea1">Example textarea</label>
     <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
