9ce12ad6d50c6030eb3460b3c70c2723eb2f70e8
maurera <42736637+maurera91@users.noreply.github.com>
2019-03-27 18:37:26 -0500

    first commit

---
+++ client/src/components/InteractionModal/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class interactionModal extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            input: ""
+        }
+    }
+
+}
+
+export default interactionModal;
