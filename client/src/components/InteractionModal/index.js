d0d43a12c289c4ebc02db8fc2c01bb544ef3ae6b
maurera <42736637+maurera91@users.noreply.github.com>
2019-03-27 19:31:11 -0500

    added drugAPI and interactionModal component

--- client/src/components/InteractionModal/index.js
+++ client/src/components/InteractionModal/index.js
@@ -1,12 +1,44 @@
 import React from "react";
+import drugAPI from "../../utils/drugAPI"
 
 class interactionModal extends React.Component {
     
     constructor(props) {
         super(props);
         this.state = {
-            input: ""
+            searchParam: "",
+            searchResult: {}
         }
+    }
+
+    componentDidUpdate = async() => {
+        let result = await drugAPI.searchInteractions(this.state.searchParam);
+        let filteredResult = result;
+        this.setState({searchResult:filteredResult})
+    }
+
+    handleSearchInputChange = (e) => {
+        const target = e.target;
+        const value = target.value;
+
+        if(value){
+            this.setState({searchParam:value})
+        }
+
+    }
+
+    render() {
+        return (
+            <div className = "modal">
+                <div className = "inputDiv">
+                    <input></input>
+                </div>
+                <div className = "resultDiv">
+                    
+                </div>
+            
+            </div>
+        )
     }
 
 }
---
+++ client/src/utils/drugAPI.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+export default function searchInteractions(name) {
+        let name = "lipitor";
+        axios.get(`https://rxnav.nlm.nih.gov/REST/rxcui?name=${name}`)
+        .then( (result) => {
+            let rxcui = result.getElementsByTagName("rxNormId")[0];
+            console.log(rxcui);
+            axios.get(`https://rxnav.nlm.nih.gov/REST/interaction/interaction.json?rxcui=${rxcui}`)
+            .then( (result) => {
+                console.log(result);
+                return result;
+            })
+        })
+}
+
+
