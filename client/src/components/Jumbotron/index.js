977da66521f12b064e182ec11ca8cb537d10394b
Dylan Dusenberry <dylandusenberry975@gmail.com>
2019-03-27 20:26:29 -0500

    styling jumbotron

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -5,7 +5,7 @@ function Jumbotron({ children }) {
   return (
     <div className="container">
     <div
-      style={{ border: "solid", height: 150, clear: "both", paddingTop: 50, textAlign: "center" }}
+      style={{ border: "solid", height: 150, clear: "both", paddingTop: 45, textAlign: "center" }}
       className="jumbotron"
     >
       {children}
