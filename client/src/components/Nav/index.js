0d4162b3263fd1ca4778ff37e6eef44bc7de380b
Dylan Dusenberry <dylandusenberry975@gmail.com>
2019-04-01 20:50:26 -0500

    nav bar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -5,6 +5,7 @@ function Nav() {
     return (
 
         <nav className="navbar navbar-expand-lg">
+        <div className="container">
             <style>
 @import url('https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300');
             </style>
@@ -14,8 +15,9 @@ function Nav() {
                 <a className="nav-item " href="/home"><i class="fa fa-home"></i>Home</a>
                 {/* <a className="nav-item " href="/scheduler/:name">Scheduler</a> */}
                 <a className="nav-item " href="/login">Login</a>
-           
+                </div>
         </nav >
+        
 
     );
 }
