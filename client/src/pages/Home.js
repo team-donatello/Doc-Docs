6effae9e37e879b8effe32892b6b5235dcdedcff
Dylan Dusenberry <dylandusenberry975@gmail.com>
2019-04-02 14:47:21 -0500

    styling

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Jumbotron from "../components/Jumbotron";
 import { Col, Row, Container } from "../components/Grid";
 import "../components/style/home.css";
-import "../components/image/green_dust_scratch.png";
+
 
 
 class Login extends Component {
@@ -11,8 +11,8 @@ class Login extends Component {
 
     render() {
         return (
+        
            
-       
             <Container fluid>
             
                 <Row>
@@ -26,7 +26,7 @@ class Login extends Component {
             </style>
                             <h1>Doc-Docs</h1>
                             <h2>Managing your schedule just got a whole lot easier.</h2>
-                             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9KAKD2BKnLB4ejeoEndPOw2xlMGbwNz64L5FPx9j2wji5WE2d"  height="170px"></img>
+                             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9KAKD2BKnLB4ejeoEndPOw2xlMGbwNz64L5FPx9j2wji5WE2d"  height="230px"></img>
 
                             
                         
